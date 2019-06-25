5909aabab6046c96c8a118c228221e16313617e8
dek-haji <deeeqhaji@gmail.com>
2019-06-24 23:08:32 -0500

    added syntax in search

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import Highlight from 'react-highlight'
+import "./Search.css"
 
 class SearchResults extends Component {
   render() {
@@ -11,7 +13,8 @@ class SearchResults extends Component {
             <Card key={result.id}>
             <Card.Content>
                   <h4>{result.title}</h4>
-                  <p>{result.note}</p>
+                  <Highlight language={"jsx"} className="react-snippet">{result.snippet}</Highlight>
+
                 </Card.Content>
                 {this.props.noteTypes.filter(note => {
                   if (note.id === result.noteTypeId) {
